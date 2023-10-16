e9326c96d23ba2e87fc67bd08315a9e5774d068c
NinoTraverso <njt98git@proton.me>
2023-10-16 13:10:58 +0000

    Added Login

--- TheLastBuildWeek/Controllers/HomeController.cs
+++ TheLastBuildWeek/Controllers/HomeController.cs
@@ -3,16 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using TheLastBuildWeek.Models;
 
 namespace TheLastBuildWeek.Controllers
 {
     public class HomeController : Controller
     {
+        private ModelDBContext db = new ModelDBContext();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Login([Bind(Include = "Username, Password, Role")] T_User users)
+        {
+
+            var user = db.T_User.FirstOrDefault(u => u.Username == users.Username && u.Password == users.Password && u.Role == "Veterinario");
+
+            if (user != null)
+            {
+
+                FormsAuthentication.SetAuthCookie(users.Username, false);
+                return RedirectToAction("LoggedIn", "Home");
+            }
+
+            return View();
+        }
+
+        //[Authorize(Roles="Veterinario")]
+        public ActionResult LoggedIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOut()
+        {
+
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ TheLastBuildWeek/Models/SiteRole.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace TheLastBuildWeek.Models
+{
+    public class SiteRole : RoleProvider
+    {
+        private ModelDBContext db = new ModelDBContext();
+
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+
+             T_User user = db.T_User.FirstOrDefault(x => x.Username == username);
+
+            List<string> roles = new List<string>();
+            roles.Add(user.Role);
+            return roles.ToArray();
+
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
