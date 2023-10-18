44c1a70abb275cd5f532a0c2d7dba67014ca611a
ImadEldinMohamed <126964061+ImadEldinMohamed@users.noreply.github.com>
2023-10-18 11:42:21 +0200

    controller farmacia

---
+++ TheLastBuildWeek/Controllers/FARMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheLastBuildWeek.Controllers
+{
+    public class FARMController : Controller
+    {
+        // GET: FARM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
