372cfc74cfe77f14dd863b7ed23dc41afe95c45d
NinoTraverso <njt98git@proton.me>
2023-10-18 14:10:18 +0000

    ricoveri rende animali con dettagli (visite list)

--- TheLastBuildWeek/Controllers/HomeController.cs
+++ TheLastBuildWeek/Controllers/HomeController.cs
@@ -111,13 +111,6 @@ namespace TheLastBuildWeek.Controllers
         }
 
         [HttpGet]
-        public ActionResult Visite()
-        {
-            return View();
-        }
-
-
-        [HttpGet]
         public ActionResult DetailVisita (T_Visita visita)
         {
             var animaleRicoverato = db.T_Animali.FirstOrDefault();
@@ -129,6 +122,15 @@ namespace TheLastBuildWeek.Controllers
             }
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult AnimaliRicoverati(T_Animali animali)
+        {
+            var dettagliRicovero = db.T_Animali.Include("T_Ricovero").ToList();
+
+
+            return View(db.T_Animali.ToList());
         }
     }
 }
