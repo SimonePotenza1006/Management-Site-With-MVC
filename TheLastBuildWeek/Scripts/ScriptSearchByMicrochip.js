b74f5137f00bc79cb60ba7bc40b5272fe1d58064
Fabio Giannasi <fabio.giannasi@outlook.com>
2023-10-18 15:00:25 +0200

    inserimento vendita con codice fiscale

--- TheLastBuildWeek/Scripts/ScriptSearchByMicrochip.js
+++ TheLastBuildWeek/Scripts/ScriptSearchByMicrochip.js
@@ -1,16 +1,1 @@
-﻿function GetByChipCode(code) {
-    $.ajax
-        ({
-            url: "Home/GetByChipCode",
-            type: 'POST',
-            success: function (item) {
-                $('#result').append("<div> <div> <img src=~/Content/images/" + item.Foto + " width='100px' height='100px'/> </div> </div>")
-            }
-        })
-}
-
-
-$('#SearchButton').click(function () {
-    let code = $("#codeInput").val()
-    GetByChipCode(code)
-}
+﻿
