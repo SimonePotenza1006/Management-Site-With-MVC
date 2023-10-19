8b1431606b42e00510aaed1fa49479453d6816ee
ImadEldinMohamed <126964061+ImadEldinMohamed@users.noreply.github.com>
2023-10-19 15:57:44 +0200

    farmcontroller index

--- TheLastBuildWeek/Controllers/FARMController.cs
+++ TheLastBuildWeek/Controllers/FARMController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace TheLastBuildWeek.Controllers
 {
+    [Authorize(Roles = "Farmacista")]
     public class FARMController : Controller
     {
+    
         // GET: FARM
         public ActionResult Index()
         {
